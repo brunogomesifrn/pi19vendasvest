073f398d3c4bf6530c0217bf7f83069ff828cd8d
MaeliSantos <maelilibnielly@gmail.com>
2019-08-19 12:41:43 -0300

    criei a url de index, a views e settings

--- pi19vendasvest/settings.py
+++ pi19vendasvest/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
+    'core',
+
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'
+
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = 'perfil'
+
+LOGOUT_REDIRECT_URL = 'index'
