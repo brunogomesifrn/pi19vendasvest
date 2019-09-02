cb228c01afc5eefe014b90a082c6b09d953e2b4c
MaeliSantos <maelilibnielly@gmail.com>
2019-09-02 12:58:53 -0300

    fiz modificacao no layout

--- core/views.py
+++ core/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'index.html')
+
+def galeria(request):
+	return render(request, 'galeria.html')
+def registro(request):
+	return render(request, 'registro.html')
+	
+def login(request):
+	return render(request, 'login.html')
+
+
+
--- pi19vendasvest/urls.py
+++ pi19vendasvest/urls.py
@@ -18,7 +18,12 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, registro, galeria, login
 urlpatterns = [
    	path('', index, name='index'),
+   	path('galeria/',galeria, name="galeria"),
+   	path('login/',login, name="login"),
+   	
+   	#registro de usuário
+   	path('registro/', registro, name='registro')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
