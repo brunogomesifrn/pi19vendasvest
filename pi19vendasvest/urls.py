fc7c797fc62e2410c22abfc986aff674d25e4ca8
Amanda474 <aamanda474@gmail.com>
2019-09-03 14:45:04 -0300

    Iniciei login

--- pi19vendasvest/urls.py
+++ pi19vendasvest/urls.py
@@ -4,13 +4,12 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path
-from core.views import index, registro, galeria, login, maquiagens
+from core.views import index, registro, galeria, maquiagens
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    	path('', index, name='index'),
    	path('galeria/',galeria, name="galeria"),
-   	path('login/',login, name="login"),
     path('maquiagens/',maquiagens, name="maquiagens"),
     path('login/', auth_views.LoginView.as_view(), name='login'),
 	#registro de usuário
