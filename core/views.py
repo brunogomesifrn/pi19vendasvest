285cde9ad0ed4eb3d9a60598d8742ed69a1c0a26
Amanda474 <aamanda474@gmail.com>
2019-09-08 09:13:12 -0300

    Algumas alteracoes em index e registro de usuario

--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
 def index(request):
 	return render(request, 'index.html')
 
@@ -15,7 +19,8 @@ def registro(request):
 		'form':form
 	}
 	return render(request, 'registro.html', contexto)
-	
+
+@login_required	
 def login(request):
 	return render(request, 'login.html')
 
@@ -34,4 +39,15 @@ def perfil(request):
 def vestuarios(request):
 	return render(request, 'vestuarios.html')
 
+@login_required	
+def dados(request, id):
+	user = User.objects.get(pk=id)
+	form = UserCreationForm(request.POST or None, instance=user)
+	if form.is_valid():
+		form.save()
+		return redirect('perfil')
+	contexto = {
+		'form': form
+	}
+	return render(request, 'registro.html', contexto)
 
--- pi19vendasvest/urls.py
+++ pi19vendasvest/urls.py
@@ -3,9 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from core.views import index, registro, galeria, login, maquiagens, perfumes, vestuarios, perfil
-
-
+from core.views import index, registro, galeria, login, maquiagens, perfumes, vestuarios, perfil, dados
 from django.contrib.auth import views as auth_views
 
 
@@ -14,24 +12,11 @@ urlpatterns = [
    	path('galeria/',galeria, name="galeria"),
     path('maquiagens/',maquiagens, name="maquiagens"),
     path('perfumes/',perfumes, name="perfumes"),
-
-   	path('perfil/', perfil, name='perfil'),
     path('vestuarios/', vestuarios, name='vestuarios'),
-
-
-   	#registro de usuário
-
-
-
     path('perfil/', perfil, name='perfil'),
-    #registro de usuário
-
-
-    path('perfil/', perfil, name='perfil'),
-
     path('login/', auth_views.LoginView.as_view(), name='login'),
-	  #registro de usuário
-    path('registro/', registro, name='registro')
+    path('registro/', registro, name='registro'),
+    path('dados/<int:id>/', dados, name='dados'),
 
    	
 
